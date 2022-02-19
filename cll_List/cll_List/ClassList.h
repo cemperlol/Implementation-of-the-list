2d87b694af3199b2777f6827e320b435323a9b8c
cemperlol <Baxeon96@yandex.ru>
2022-02-20 02:00:50 +0300

    New methods were added
    New methods:
    – reverse
    – sort
    – unique
    – assign
    – remove

--- cll_List/cll_List/ClassList.h
+++ cll_List/cll_List/ClassList.h
@@ -1,5 +1,5 @@
 namespace Lst
-{
+{	
 	template <typename T>
 	class cll_List
 	{
@@ -9,16 +9,21 @@ namespace Lst
 
 		void push_back(T data);
 		void pop_back();
-		int GetSize();
+		int get_size();
 		void push_front(T data);
 		void pop_front();
-		void insert(T data, int index);
+		void insert(T data, const int unsigned& index);
 		void clear();
-		void show_all();
-		void remove_at(const int index);
-		void random_fill(const unsigned int quantity);
-		void change_data(const int index1, const int index2, bool unused);
-		void change_data(const int data, const int index);
+		void print();
+		void erase(const unsigned int& index);
+		void random_fill(const unsigned int& quantity);
+		void change_data(const unsigned int& index1, const unsigned int& index2, const bool& unused);
+		void change_data(const T data, const unsigned int& index);
+		void reverse();
+		void sort(bool ReverseMode = false);
+		void remove(const T data);
+		void unique();
+		void assign(const unsigned int& quantity, const T data);
 
 		T& operator[](const int index);
 
@@ -40,9 +45,9 @@ namespace Lst
 
 			Node& operator =(const Node& NodeToCopy)
 			{
-				this->data = NodeToCopy->data;
-				this->pNext = NodeToCopy->pNext;
-				this->pPrev = NodeToCopy->pPrev;
+				this->data = NodeToCopy.data;
+				this->pNext = NodeToCopy.pNext;
+				this->pPrev = NodeToCopy.pPrev;
 
 				return *this;
 			}
@@ -53,9 +58,10 @@ namespace Lst
 
 		unsigned int Size;
 		Node<T>* Head;
-		Node<T>* Tale;
+		Node<T>* Tail;
 
-		Node<T>* search_needed_node(const int index);
+		Node<T>* search_needed_node(const unsigned int index);
+		void exchange_chosen_nodes(int& FirstData, int& SecondData);
 
 	};
 
@@ -63,7 +69,7 @@ namespace Lst
 	cll_List<T>::cll_List()
 	{
 		Size = 0;
-		Tale = Head = nullptr;
+		Tail = Head = nullptr;
 	}
 
 	template<typename T>
@@ -75,13 +81,13 @@ namespace Lst
 	template<typename T>
 	void cll_List<T>::push_back(T data)
 	{
-		if (Tale == nullptr)
+		if (Tail == nullptr)
 		{
-			Head = Tale = new Node<T>(data);
+			Head = Tail = new Node<T>(data);
 		}
 		else
 		{
-			Tale = Tale->pNext = new Node<T>(data, nullptr, Tale);
+			Tail = Tail->pNext = new Node<T>(data, nullptr, Tail);
 		}
 
 		Size++;
@@ -90,21 +96,21 @@ namespace Lst
 	template<typename T>
 	void cll_List<T>::pop_back()
 	{
-		if (Tale == nullptr || Tale->pPrev == nullptr)
+		if (Tail == nullptr || Tail->pPrev == nullptr)
 		{
 		}
 		else
 		{
-			if (Tale->pPrev != nullptr)
+			if (Tail->pPrev != nullptr)
 			{
-				Tale = Tale->pPrev;
-				delete Tale->pNext;
-				Tale->pNext = nullptr;
+				Tail = Tail->pPrev;
+				delete Tail->pNext;
+				Tail->pNext = nullptr;
 			}
 			else
 			{
-				delete Tale;
-				Head = Tale = nullptr;
+				delete Tail;
+				Head = Tail = nullptr;
 			}
 
 			Size--;
@@ -112,7 +118,7 @@ namespace Lst
 	}
 
 	template<typename T>
-	int cll_List<T>::GetSize()
+	int cll_List<T>::get_size()
 	{
 		return this->Size;
 	}
@@ -122,7 +128,7 @@ namespace Lst
 	{
 		if (Head == nullptr)
 		{
-			Head = Tale = new Node<T>(data);
+			Head = Tail = new Node<T>(data);
 		}
 		else
 		{
@@ -149,7 +155,7 @@ namespace Lst
 			else
 			{
 				delete Head;
-				Tale = Head = nullptr;
+				Tail = Head = nullptr;
 			}
 
 			Size--;
@@ -167,14 +173,14 @@ namespace Lst
 				delete Head->pPrev;
 			}
 			delete Head;
-			Head = nullptr;
+			Head = Tail = nullptr;
 		}
 
 		Size = 0;
 	}
 
 	template<typename T>
-	void cll_List<T>::show_all()
+	void cll_List<T>::print()
 	{
 		if (Size == 0)
 		{
@@ -193,7 +199,7 @@ namespace Lst
 	}
 
 	template<typename T>
-	void cll_List<T>::remove_at(int index)
+	void cll_List<T>::erase(const unsigned int& index)
 	{
 		Node<T>* Current;
 
@@ -218,16 +224,16 @@ namespace Lst
 	}
 
 	template<typename T>
-	void cll_List<T>::random_fill(const unsigned int quantity)
+	void cll_List<T>::random_fill(const unsigned int& quantity)
 	{
 		for (int i = 0; i < quantity; i++)
 		{
-			push_back((T)rand());
+			push_back((T)rand()%10);
 		}
 	}
 
 	template<typename T>
-	void cll_List<T>::change_data(const int index1, const int index2, bool unused)
+	void cll_List<T>::change_data(const unsigned int& index1, const unsigned int& index2, const bool& unused)
 	{
 		Node<T>* FirstNode, * SecondNode;
 		int TempData;
@@ -240,14 +246,134 @@ namespace Lst
 	}
 
 	template<typename T>
-	void cll_List<T>::change_data(const int data, const int index)
+	void cll_List<T>::change_data(const T data, const unsigned int& index)
 	{
 		Node<T>* FirstNode = search_needed_node(index);
 		FirstNode->data = data;
 	}
 
 	template<typename T>
-	void cll_List<T>::insert(T data, int index)
+	void cll_List<T>::reverse()
+	{
+		Node<T>* FirstNode, * SecondNode;
+		FirstNode = Head;
+		SecondNode = Tail;
+		exchange_chosen_nodes(FirstNode->data, SecondNode->data);
+
+		for (int i = 1; i < (int)this->Size / 2; i++)
+		{
+			FirstNode = FirstNode->pNext;
+			SecondNode = SecondNode->pPrev;
+
+			exchange_chosen_nodes(FirstNode->data, SecondNode->data);
+		}
+	}
+
+	template<typename T>
+	void cll_List<T>::sort(bool ReverseMode)
+	{
+		bool Unsorted;
+		T Temp = 0;
+		Node<T>* FirstNode;
+
+
+		do
+		{
+			Unsorted = false;
+			FirstNode = Head;
+
+			while (FirstNode->pNext != nullptr)
+			{
+				if (FirstNode->data > FirstNode->pNext->data)
+				{
+					Temp = FirstNode->data;
+					FirstNode->data = FirstNode->pNext->data;
+					FirstNode->pNext->data = Temp;
+
+					Unsorted = true;
+				}
+
+				FirstNode = FirstNode->pNext;
+			}
+		} while (Unsorted);
+
+		if (ReverseMode)
+		{
+			reverse();
+		}
+	}
+
+	template<typename T>
+	void cll_List<T>::remove(const T data)
+	{
+		Node<T>* Current = Head, * TempNode;
+
+		do
+		{
+			if (Current->data == data)
+			{
+				if (Current->pNext != nullptr) { Current->pNext->pPrev = Current->pPrev; }
+				Current->pPrev->pNext = Current->pNext;
+				TempNode = Current->pNext;
+				delete Current;
+				
+				Current = TempNode;
+				Size--;
+			}
+			else
+			{
+				Current = Current->pNext;
+			}
+			
+		} while (Current != nullptr);
+	}
+
+	template<typename T>
+	void cll_List<T>::unique()
+	{
+		Node<T>* Current = Head, * TempNode;
+		int count = 0;
+
+		do
+		{
+			if (Current->data == Current->pNext->data)
+			{
+				count++;
+
+				if (count > 0)
+				{
+					Current->pNext->pPrev = Current->pPrev; 
+					Current->pPrev->pNext = Current->pNext;
+					TempNode = Current->pPrev;
+					delete Current;
+
+					Current = TempNode;
+					Size--;
+				}
+			}
+			else
+			{
+				count = 0;
+			}
+			
+			Current = Current->pNext;
+		} while (Current->pNext != nullptr);
+	}
+
+	template<typename T>
+	void cll_List<T>::assign(const unsigned int& quantity, const T data)
+	{
+		clear();
+		Size = 0;
+
+		for (int i = 0; i < quantity; i++)
+		{
+			push_back(data);
+		}
+	}
+
+	template<typename T>
+	void cll_List<T>::insert(T data, const int unsigned& index)
 	{
 		Node<T>* Current;
 
@@ -285,7 +411,7 @@ namespace Lst
 		}
 		else
 		{
-			Current = Tale;
+			Current = Tail;
 			count = Size - 1;
 
 			while (count > index)
@@ -299,7 +425,7 @@ namespace Lst
 	}
 
 	template<typename T>
-	cll_List<T>::Node<T>* cll_List<T>::search_needed_node(const int index)
+	cll_List<T>::Node<T>* cll_List<T>::search_needed_node(const unsigned int index)
 	{
 		{
 			Node<T>* NeededNode;
@@ -315,7 +441,7 @@ namespace Lst
 			}
 			else
 			{
-				NeededNode = Tale;
+				NeededNode = Tail;
 
 				for (int i = Size - 1; i > index; i--)
 				{
@@ -325,5 +451,13 @@ namespace Lst
 
 			return NeededNode;
 		}
+	}
+
+	template<typename T>
+	void cll_List<T>::exchange_chosen_nodes(int& FirstData, int& SecondData)
+	{
+		int TempData = FirstData;
+		FirstData = SecondData;
+		SecondData = TempData;
 	}
 }
--- cll_List/cll_List/Source.cpp
+++ cll_List/cll_List/Source.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include "ClassList.h"
-
-using namespace Lst;
-using namespace std;
 /*
 *************************************************************INFO************************************************************
 Class cll_List is my own implementation of STL list. This class is generic so don't forget to point data type
@@ -12,16 +7,27 @@ List_Name.push_back(value) - adds value to the end of the list
 List_Name.pop_back() - removes last Node from the list
 List_Name.push_front(value) - adds value to the beginning of the list
 List_Name.pop_frongt() - removes first Node from the list
-List_Name.insert_at(value, index) - adds value at the chosen index (doest change it)
-List_Name.remove_at(index) - removes Node at the chosen index
+List_Name.insert(value, index) - adds value at the chosen index (doest change it)
+List_Name.erase(index) - removes Node at the chosen index
 List_Name.random_fill(quantity) - adds chosen amount of random numbers to the list (doesnt work with user's data types and string)
-List_Name.show_all() - shows all the data from all Nodes
+List_Name.print() - shows all the data from all Nodes
 List_Name.clear() - removes all Nodes in the list
-List_Name[index] - returns data of a specific Node
 List_Name.GetSize() - returns the number of Nodes in the list
 List_Name.change_data(data, index) - changes data in the chosen Node
 List_Name.change_data(index1, index2, unused) - exchanges data between two Nodes (parametr unused's being used to differ overloads)
+List_Name.reverse() - reverses the order of Nodes
+List_Name.unique() - deletes all Nodes with the same date, which come in the row
+List_Name.remove(value) - removes all Nodes with the specified value
+List_Name.sort(ReverseMode) - sorts all Nodes depending on their data in chosen way (direct or reverse)
+List_Name.assign(quantity, value) - removes old Nodes and creates new ones instead with a specific value
+List_Name[index] - returns data of a specific Node
 */
+
+#include <iostream>
+#include "ClassList.h"
+
+using namespace Lst;
+using namespace std;
 
 int main()
 {
